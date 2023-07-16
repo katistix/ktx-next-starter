54524802274ec866eccdddb17ad9011bca102912
Paul <2018.tpg@gmail.com>
2023-07-16 15:03:47 +0300

    shad cn init

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/"] }
+const protectedRoutes = [
+    "/"
+]
+
+export const config = { matcher: protectedRoutes }
